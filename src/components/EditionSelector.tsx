51b69df8ffb9f207133a1c129a33716e7697f2f5
Your Name <you@example.com>
2025-08-27 21:37:39 -0400

    Updated src/components/EditionSelector.tsx

--- src/components/EditionSelector.tsx
+++ src/components/EditionSelector.tsx
@@ -53,7 +53,7 @@ export function EditionSelector({
   selectedLanguage,
 }: EditionSelectorProps) {
   const buttonBaseClasses =
-    "uppercase font-jetbrains-mono font-light text-xs text-white rounded-md p-4 text-center transition-opacity duration-300 ease-in-out focus:outline-none border-none";
+    "uppercase font-jetbrains-mono font-light text-sm text-white rounded-md p-4 text-center transition-opacity duration-300 ease-in-out focus:outline-none border-none";
 
   return (
     <Card>
