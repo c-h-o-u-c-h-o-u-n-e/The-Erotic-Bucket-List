bdc2de4a6b5a231270c09a3b08b733719caca303
Your Name <you@example.com>
2025-08-27 20:40:22 -0400

    Updated src/app/page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import { EroticBucketListHeader } from "@/components/EroticBucketListHeader";
 export default function Home() {
   return (
     <div className="grid grid-rows-[1fr_20px] items-center justify-items-center min-h-screen p-0 pb-4 sm:p-4 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-1 items-center sm:items-start">
+      <main className="flex flex-col gap-0 row-start-1 items-center sm:items-start">
         <EroticBucketListHeader />
       </main>
       <MadeWithDyad />
